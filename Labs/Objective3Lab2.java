8d26f790500f8aa401b539d87f75068a616623b4
Gabby <glsbutterfield@gmail.com>
2024-10-11 22:22:43 -0600

    initial commit

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,13 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		double side1, side2, sq_side1, sq_side2, hypotenuse;
+
+		side1 = 10;
+		side2 = 8;
+		sq_side1 = side1 * side1;
+		sq_side2 = side2 * side2;
+		hypotenuse = Math.sqrt(sq_side1 + sq_side2);
+		
+System.out.println(hypotenuse); 
+}
+	}
