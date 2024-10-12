385cbe703346ff9350609df2b8c9b03261666512
Gabby <glsbutterfield@gmail.com>
2024-10-12 14:16:46 -0600

    initial commit

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4 {
+	public static void main (String[] args) {
+		int sum = 0;
+		int count = 0;
+
+		do{
+			sum = sum+count;
+			count++;
+		}
+		while (count <= 20);{
+System.out.println(sum);
+		}
+	}
+}
