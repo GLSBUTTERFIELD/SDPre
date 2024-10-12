aaa1d607b75ebf332bf7d558e6f0684ba86074f1
Gabby <glsbutterfield@gmail.com>
2024-10-12 11:38:14 -0600

    initial commit

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Name: Gabby Butterfield";
+    String agee = "Age:";
+    int age = 35;
+    String state = "State: Korea";
+
+System.out.println(name + " " + agee + " " + age + " " + state);
+
+  }
+}
